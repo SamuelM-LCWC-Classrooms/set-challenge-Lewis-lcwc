794bb1f0bb8288a6e571796c76e258c638dedc01
Lewis-lcwc <30075425@lcwc.ac.uk>
2024-09-25 09:59:38 +0000

    setstask

--- task.py
+++ task.py
@@ -2,9 +2,12 @@ def task(): # Hint - you may need to create new, mid-point sets for the individu
     fruits_1 = {"apple", "banana", "cherry", "grape", "mango", "pineapple", "papaya","sprite", "orange", "lemon", "strawberry"}
     fruits_2 = {"raspberry", "banana", "cherry", "grape", "mango", "blueberry", "papaya", "melon", "lemon", "steak"}
 
-    duplicate_fruits = None # This should be a tuple containing all the fruits in both tuples
-    individual_fruits = None # This should be a tuple containing only the individual fruits
+fruits_1.remove("sprite")
+fruits_2.remove("steak")
+    duplicate_fruits = (fruits_1.intersection(fruits_2)) # This should be a tuple containing all the fruits in both tuples
+    individual_fruits = (fruits_1.symmetric_difference(fruits_2)) # This should be a tuple containing only the individual fruits
 
 
     return [duplicate_fruits, individual_fruits] # Note - functions can only return one data item - so both tuples
                                                  # are contained inside a single list
+ print(task))                                                
